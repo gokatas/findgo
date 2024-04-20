b95ce37b01366faf6a16b63d8fcc89f1262dde2b
Jozef Reisinger <jozef.reisinger@gmail.com>
2024-04-20 22:25:58 +0200

    first commit

---
+++ findgo_test.go
@@ -0,0 +1,29 @@
+package findgo_test
+
+import (
+	"findgo"
+	"testing"
+	"testing/fstest"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestFilesCorrectlyListsInMemoryFiles(t *testing.T) {
+	t.Parallel()
+	fsys := fstest.MapFS{
+		"file.go":                {},
+		"subfolder/subfolder.go": {},
+		"subfolder2/another.go":  {},
+		"subfolder2/file.go":     {},
+	}
+	want := []string{
+		"file.go",
+		"subfolder/subfolder.go",
+		"subfolder2/another.go",
+		"subfolder2/file.go",
+	}
+	got := findgo.Files(fsys)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
